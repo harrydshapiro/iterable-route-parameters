a9e1fb4769ffa732c585337f210b2145b30b2036
Harry Shapiro <harrydshapiro96@gmail.com>
2021-08-12 14:29:58 -0400

    built

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,22 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var defaultInitializationOptions = {
+    delimiter: '+'
+};
+var iterableRouteParamters = function (customInitializationOptions) { return function (request, response, next) {
+    var initializationOptions = Object.assign({}, defaultInitializationOptions, customInitializationOptions);
+    var parsedParams = {};
+    for (var param in request.params) {
+        var delimiter = initializationOptions.delimiter;
+        var originalValue = request.params[param];
+        if (originalValue.indexOf(initializationOptions.delimiter)) {
+            parsedParams[param] = originalValue.split(delimiter);
+        }
+        else {
+            parsedParams[param] = originalValue;
+        }
+    }
+    request.parsedParams = parsedParams;
+    next();
+}; };
+exports.default = iterableRouteParamters;
